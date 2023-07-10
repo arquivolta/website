ce5bfc61e0afb769f7bc66e901b650e323b87be2
Anaïs Betts <anais@anaisbetts.org>
2023-07-10 21:35:17 +0200

    Initial redirects

--- app/api/redirect/route.ts
+++ app/api/redirect/route.ts
@@ -1,6 +1,18 @@
 import { NextResponse, NextRequest } from "next/server";
 
+const redirectTable: Record<string, string> = {
+  "arch-shasums-x8664":
+    "https://mirror.rackspace.com/archlinux/iso/latest/sha256sums.txt",
+  "arch-arm64-image":
+    "https://os.archlinuxarm.org/os/ArchLinuxARM-aarch64-latest.tar.gz",
+};
+
+const defaultRedirect = "https://arquivolta.dev";
+
+// Rather than ever hard-coding 3rd-party URLs in the desktop app or other places,
+// always provide this redirect so that if that site ever goes pear-shaped, we can
+// shim it server-side
 export function GET(request: NextRequest) {
 	let q = request.nextUrl.searchParams.get('q');
-	return NextResponse.redirect(q = "ddg" ? "https://www.duckduckgo.com" : "https://google.com");
+	return NextResponse.redirect(redirectTable[q ?? '__nothere'] || defaultRedirect);
 }
